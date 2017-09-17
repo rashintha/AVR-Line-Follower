717171af1671f9c1363c92d2ab58b8ca04262dd7
Rashintha Maduneth <rashinthamaduneth@gmail.com>
2017-09-17 18:10:34 +0530

    Realtime Plotting with Python

--- realtime_plot.py
+++ realtime_plot.py
@@ -9,15 +9,6 @@ plotList = [1, 2, 3, 4, 5, 6, 7, 8]
 while True:
     with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as ser:
         x = ser.read()
-        val = 0
-        val = val | 1 if (1 & ord(x)) else val
-        val = val | 2 if (2 & ord(x)) else val
-        val = val | 4 if (4 & ord(x)) else val
-        val = val | 8 if (8 & ord(x)) else val
-        val = val | 16 if (16 & ord(x)) else val
-        val = val | 32 if (32 & ord(x)) else val
-        val = val | 64 if (64 & ord(x)) else val
-        val = val | 128 if (128 & ord(x)) else val
 
         valList = []
 
@@ -45,15 +36,13 @@ while True:
         if not (128 & ord(x)):
             valList.append(8)
 
-        print(valList)
-
-        #print(bin(val))
-        #print(1 & ord(x), 2 & ord(x), 4 & ord(x), 8 & ord(x), 16 & ord(x), 32 & ord(x), 64 & ord(x), 128 & ord(x))
-
         plotList = plotList + valList
         print(plotList)
+
+        # calculating mean
         mu = sum(plotList) / len(plotList)
 
+        # calculating standard deviation
         tempList = [math.pow(element - mu, 2) for element in plotList]
         sigma = math.sqrt(sum(tempList) / (len(plotList) - 1))
 
